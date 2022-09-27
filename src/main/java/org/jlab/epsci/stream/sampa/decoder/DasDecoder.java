d8effec2df10607de03e7441a01129dbde959269
gurjyan <gurjyan@jlab.org>
2022-09-27 11:26:23 -0400

    debugging

--- src/main/java/org/jlab/epsci/stream/sampa/decoder/DasDecoder.java
+++ src/main/java/org/jlab/epsci/stream/sampa/decoder/DasDecoder.java
@@ -748,7 +748,7 @@ public class DasDecoder implements SDecoder {
                 // This will fit into a short w/out having to worry about sign extension.
 //if (streamId == 2)  System.out.println("get stream " + streamId + " at " + (offset + channel * 2 + 1));
                 short adc_value = (short) (((data.get(offset + channel * 2 + 1)) << 5) | (data.get(offset + channel * 2)));
-if ((id == 1) && (channel == 0) && (streamId == 2) && numSamples < 100) {
+if ((id == 1) && (channel == 0) && (streamId == 2) && numSamples < 20) {
                 System.out.print(Integer.toHexString(adc_value) + "  " );
 }
 //                System.out.println("stream = "+ streamId + ", channel = "+ channel+" "+ Integer.toHexString(adc_value));
