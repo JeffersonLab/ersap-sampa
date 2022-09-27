9354dd4bde9cd893a4e0d563f0248bd8e1b48f86
gurjyan <gurjyan@jlab.org>
2022-09-27 10:03:03 -0400

    debugging

--- src/main/java/org/jlab/epsci/stream/sampa/decoder/DasDecoder.java
+++ src/main/java/org/jlab/epsci/stream/sampa/decoder/DasDecoder.java
@@ -712,7 +712,8 @@ public class DasDecoder implements SDecoder {
 //if (streamId == 2)  System.out.println("get stream " + streamId + " at " + (offset + channel * 2 + 1));
                 short adc_value = (short)(((data.get(offset + channel * 2 + 1)) << 5) | (data.get(offset + channel * 2)));
 //if ((id == 1) && (channel == 0) && (streamId == 2) && numSamples < 10) {
-if (numSamples < 10) {
+//                System.out.print(Integer.toHexString(adc_value) + "  " );
+                if (numSamples < 10) {
     System.out.println("streamId = "+ streamId +" "+ Integer.toHexString(adc_value) + "  " );
 }
                 // Read this into a SRingRawEvent, not into local memory ...
