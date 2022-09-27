1a794858dff202574d3878c76004eb36dcbbdcfe
gurjyan <gurjyan@jlab.org>
2022-09-27 09:43:29 -0400

    debugging

--- src/main/java/org/jlab/epsci/stream/sampa/decoder/DasDecoder.java
+++ src/main/java/org/jlab/epsci/stream/sampa/decoder/DasDecoder.java
@@ -711,9 +711,9 @@ public class DasDecoder implements SDecoder {
                 // This will fit into a short w/out having to worry about sign extension.
 if (streamId == 2)  System.out.println("get stream " + streamId + " at " + (offset + channel * 2 + 1));
                 short adc_value = (short)(((data.get(offset + channel * 2 + 1)) << 5) | (data.get(offset + channel * 2)));
-if ((id == 1) && (channel == 0) && (streamId == 2) && numSamples < 10) {
+//if ((id == 1) && (channel == 0) && (streamId == 2) && numSamples < 10) {
     System.out.print(Integer.toHexString(adc_value) + "  " );
-}
+//}
                 // Read this into a SRingRawEvent, not into local memory ...
                 try {
                     // The buffers in the raw event object are all set to little endian
