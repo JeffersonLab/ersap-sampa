957bc9d5d9b372cffa42c4cb4e389de10bd97080
gurjyan <gurjyan@jlab.org>
2022-09-28 10:09:41 -0400

    debugging

--- src/main/java/org/jlab/epsci/stream/engine/SampaStreamTestEngine.java
+++ src/main/java/org/jlab/epsci/stream/engine/SampaStreamTestEngine.java
@@ -62,7 +62,10 @@ public class SampaStreamTestEngine implements Engine {
 //                " order="+data.order() +
 //                " direct="+data.isDirect()+
 //                " readonly="+data.isReadOnly());
-        System.out.println("data = "+data.getInt()+" "+data.getInt()+" "+data.getInt());
+        System.out.println("data = "+Integer.toHexString(data.getInt())+
+                " "+Integer.toHexString(data.getInt())+
+                " "+Integer.toHexString(data.getInt())+
+                " "+Integer.toHexString(data.getInt()));
         return input;
     }
 
