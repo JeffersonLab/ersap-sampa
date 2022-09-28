a4372863b5e2d6805e7474aa2479ec9b8cbd5195
gurjyan <gurjyan@jlab.org>
2022-09-28 15:54:25 -0400

    debugging

--- src/main/java/org/jlab/epsci/stream/engine/SampaWriteEngine.java
+++ src/main/java/org/jlab/epsci/stream/engine/SampaWriteEngine.java
@@ -25,7 +25,7 @@ public class SampaWriteEngine extends AbstractEventWriterService<FileWriter> {
     @Override
     protected FileWriter createWriter(Path file, JSONObject opts)
             throws EventWriterException {
-        System.out.println("DDD "+file);
+        System.out.println("DDD ===================================== "+file);
         try {
             return new FileWriter(file.toString());
         } catch (IOException e) {
